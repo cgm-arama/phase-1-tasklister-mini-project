e8c4eb0cce033bed7b43a529aff9655b70c7db71
Eric <eric.arama@student.moringaschool.com>
2023-04-12 11:08:13 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myButton = document.getElementById('myButton');
+myButton.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the button from refreshing the page
+  const myInput = document.getElementById('myInput');
+  const inputValue = myInput.value;
+  console.log(inputValue); // Output the value of the input to the console
+});
+
 });
